73eafe694cece64dd08d438615e431515e123dfe
Wonjeong Park <75118947+ParkWonjeong@users.noreply.github.com>
2024-08-01 23:22:15 +0900

    Feat: 디자인 및 여러 장르, 시간대 관련  태그 추가

--- gamemate-frontend/src/pages/Home/Home.jsx
+++ gamemate-frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Modal, Typography, Button, IconButton } from '@mui/material';
+import { Box, Modal, Typography, Divider, Button, IconButton } from '@mui/material';
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 import profilePlaceholder from '../../assets/profile_placeholder.png';
 import './Home.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -38,90 +42,157 @@ const Home = () => {
     users[(currentIndex + 1) % users.length]
   ];
 
+  const banners = [
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFui3t%2FbtqLGYa9nYl%2FrjBvHRfM11PVO1cVyHmyl1%2Fimg.jpg',
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FqZhFG%2FbtqLGYPLiBS%2Fc9cNPA1RJNCYhw8bLhHlSK%2Fimg.png',
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbza9gv%2FbtqL2wykAuZ%2FKhtxVo2RlmPiS5szqs2Q91%2Fimg.png',
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+
+
   return (
     <Box>
+      <Divider sx={{ backgroundColor: 'rgba(128, 128, 128, 0.3)', width: '100%', mb: 2 }} />
       <Box>
-        <Typography
-          variant="h6"
-          sx={{
-            fontFamily: 'Roboto, sans-serif',
-            fontWeight: 700,
-            fontSize: '14pt',
-            letterSpacing: '-0.5px',
-            marginBottom: '20px',
-          }}
-        >
-        🕹️ 오늘의 추천 게임메이트
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: 'Roboto, sans-serif',
-            fontWeight: 700,
-            fontSize: '10pt',
-            letterSpacing: '-0.5px',
-            marginBottom: '20px',
-          }}>
-          <a href="/recommend" className="more-link">더 보기 &gt;</a>
-        </Typography>
+        <Box display={"flex"} alignContent={"center"} justifyContent={"space-between"}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: 'Roboto, sans-serif',
+              fontWeight: 700,
+              fontSize: '14pt',
+              letterSpacing: '-0.5px',
+              marginBottom: '20px',
+            }}
+          >
+          🕹️ 오늘의 추천 게임메이트
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: 'Roboto, sans-serif',
+              fontWeight: 700,
+              fontSize: '10pt',
+              letterSpacing: '-0.5px',
+              marginBottom: '20px',
+            }}>
+            <a href="/recommend" className="more-link">더 보기 &gt;</a>
+          </Typography>
+        </Box>
       </Box>
       <Box display="flex" alignItems="center" justifyContent="center" position="relative" width="100%">
-        <IconButton onClick={handlePrev} className="carousel-button" sx={{ position: 'absolute', left: '0', zIndex: 1 }}>
+        <Box>
+        <IconButton onClick={handlePrev} className="carousel-button" sx={{ position: 'absolute', left: '15%', top: '20%', zIndex: 2 }}>
           <ArrowBackIos />
         </IconButton>
-        <Box display="flex" justifyContent="center" alignItems="center" className="user-card-container" mx={1}>
-            {visibleUsers.map((user, index) => (
-              user && (
-                <Box
-                  key={index}
-                  className={`user-card ${index === 1 ? 'active' : 'inactive'}`}
-                  onClick={index === 1 ? handleOpen : null}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    border: 'none',
-                    marginTop: '20px',
-                    boxShadow: 'none',
-                  }}
-                >
-                  <img
-                    src={profilePlaceholder}
-                    alt="Profile"
-                    className="profile-pic"
-                    style={{
-                      transform: index === 1 ? 'scale(1.1)' : 'scale(0.9)',
-                      opacity: index === 1 ? 1 : 0.6,
-                      transition: 'transform 0.3s, opacity 0.3s',
-                      marginLeft: index !== 0 ? '10px' : '14px',
-                    }}
-                  />
-                  <Typography
-                    variant="body1"
+          <Box display="flex" justifyContent="center" alignItems="center" className="user-card-container" mx={1}>
+              {visibleUsers.map((user, index) => (
+                user && (
+                  <Box
+                    key={index}
+                    className={`user-card ${index === 1 ? 'active' : 'inactive'}`}
+                    onClick={index === 1 ? handleOpen : null}
                     sx={{
-                      fontFamily: 'Roboto, sans-serif',
-                      fontWeight: 700,
-                      fontSize: index === 1 ? '14pt' : '10pt',
-                      letterSpacing: '-0.5px',
-                      marginTop: '10px',
-                      textAlign: 'center',
-                      color: index !== 1 ? 'gray' : 'black',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      border: 'none',
+                      marginTop: '20px',
+                      boxShadow: 'none',
                     }}
-                    className="username"
                   >
-                    {user.recommend_user}
-                  </Typography>
-                </Box>
-              )
-            ))}
-        </Box>
-        <IconButton onClick={handleNext} className="carousel-button" sx={{ position: 'absolute', right: '0', zIndex: 1 }}>
-          <ArrowForwardIos />
-        </IconButton>
+                    <img
+                      src={profilePlaceholder}
+                      alt="Profile"
+                      className="profile-pic"
+                      style={{
+                        transform: index === 1 ? 'scale(1.7)' : 'scale(0.9)',
+                        opacity: index === 1 ? 1 : 0.6,
+                        transition: 'transform 0.3s, opacity 0.3s',
+                        marginLeft: index !== 0 ? '10px' : '14px',
+                        marginBottom: index === 1 ? '40px' : '0px',
+                      }}
+                    />
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        fontFamily: 'Roboto, sans-serif',
+                        fontWeight: 700,
+                        fontSize: index === 1 ? '14pt' : '10pt',
+                        letterSpacing: '-0.5px',
+                        marginTop: '10px',
+                        textAlign: 'center',
+                        color: index !== 1 ? 'gray' : 'black',
+                      }}
+                      className="username"
+                    >
+                      {user.recommend_user}
+                    </Typography>
+                  </Box>
+                )
+              ))}
+          </Box>
+          <IconButton onClick={handleNext} className="carousel-button" sx={{ position: 'absolute', right: '15%', top: '20%', zIndex: 2 }}>
+            <ArrowForwardIos />
+          </IconButton>
+        <Box>
+          {visibleUsers.map((user, index) => (
+                user && (
+                  <Box
+                    key={index}
+                    className={`user-card ${index === 1 ? 'active' : 'inactive'}`}
+                    onClick={index === 1 ? handleOpen : null}
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      border: 'none',
+                      marginTop: '-50px',
+                      boxShadow: 'none',
+                    }}
+                  >
+                    {index === 1 && (
+                    <><Box sx={{ mt: 2 }}>
+                        {visibleUsers[1].common_genre.map((genre, index) => (
+                          <span key={index} className="tag3">{genre}</span>
+                        ))}
+                      </Box>
+                      <Box className="modal-tags">
+                          {visibleUsers[1].common_play_time.map((time, index) => (
+                            <span key={index} className="tag4">{time}</span>
+                          ))}
+                        </Box></>
+                  )}
+                  </Box>
+                )
+              ))}
+              </Box>
+          </Box>
       </Box>
-      <Box my={2} display="flex" justifyContent="center" width="100%">
-        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFui3t%2FbtqLGYa9nYl%2FrjBvHRfM11PVO1cVyHmyl1%2Fimg.jpg" alt="광고 배너" className="ad-banner-img" />
-      </Box>
+      <Divider sx={{ backgroundColor: 'rgba(128, 128, 128, 0.3)', width: '100%', mb: 2 }} />
+      <Slider {...settings}>
+                {banners.map((banner, index) => (
+                    <Box
+                        key={index}
+                        component="img"
+                        src={banner}
+                        alt={`Banner ${index + 1}`}
+                        sx={{ width: '100%', height: 'auto' }}
+                    />
+                ))}
+            </Slider>
+
       <Modal
         open={open}
         onClose={handleClose}
@@ -156,7 +227,7 @@ const Home = () => {
                   letterSpacing: '-0.5px',
                 }}>공통 장르</Typography>
                 {visibleUsers[1].common_genre.map((genre, index) => (
-                  <span key={index} className="tag">{genre}</span>
+                  <span key={index} className="tag1">{genre}</span>
                 ))}
               </Box>
               <Box className="modal-tags">
@@ -167,7 +238,7 @@ const Home = () => {
                   letterSpacing: '-0.5px',
                 }}>공통 시간대</Typography>
                 {visibleUsers[1].common_play_time.map((time, index) => (
-                  <span key={index} className="tag">{time}</span>
+                  <span key={index} className="tag2">{time}</span>
                 ))}
               </Box>
               <Box mt={2}>
--- gamemate-frontend/src/pages/Recommend/Recommend.jsx
+++ gamemate-frontend/src/pages/Recommend/Recommend.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Typography, Avatar, Paper, Chip, Grid, IconButton } from '@mui/material';
-import { ChatBubbleOutline, PersonAdd } from '@mui/icons-material';
+import { Box, Typography, Paper, Chip, Divider, Grid, IconButton, Button } from '@mui/material';
+import { ChatBubbleOutline, PersonAdd, ArrowBack  } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 import profilePlaceholder from '../../assets/profile_placeholder.png';
 import './Recommend.css';
 
 const Recommend = () => {
   const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -21,6 +23,16 @@ const Recommend = () => {
 
   return (
     <Box className="recommend-container">
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<ArrowBack />}
+        onClick={() => navigate('/')}
+        sx={{ marginBottom: '20px' }}
+      >
+        뒤로 가기
+      </Button>
+      <Divider sx={{ backgroundColor: 'rgba(128, 128, 128, 0.3)', width: '100%', mb: 2 }} />
       <Typography variant="h6"
         sx={{
           fontFamily: 'Roboto, sans-serif',
@@ -31,42 +43,43 @@ const Recommend = () => {
         }}>
          🕹️ 오늘의 추천 게임메이트
       </Typography>
+      
       {users.map((user, index) => (
         <Paper key={index} className="user-card">
           <Grid container alignItems="center" spacing={2}>
             <Grid item>
-              <Avatar src={profilePlaceholder} alt="Profile" className="profile-pic" />
+              <img src={profilePlaceholder} alt="Profile" className="profile-pic" />
             </Grid>
             <Grid item xs>
-              <Typography variant="h6"
-                sx={{
-                fontFamily: 'Roboto, sans-serif',
-                fontWeight: 600,
-                fontSize: '12pt',
-                letterSpacing: '-0.5px',
-                marginBottom: '20px',
-                }} className="username">
-                {user.recommend_user}
-              </Typography>
-              <Box className="tags">
+              <Box display={"flex"} justifyContent={"space-between"} alignContent={"center"} marginBottom={"7px"}>
+                <Typography variant="h6"
+                  sx={{
+                  fontFamily: 'Roboto, sans-serif',
+                  fontWeight: 800,
+                  fontSize: '12pt',
+                  letterSpacing: '-0.5px',
+                  marginTop: '5px'
+                  }} className="username">
+                  {user.recommend_user}
+                </Typography>
+                <Box className="icon-buttons" display={"flex"} alignContent={'center'}>
+                  <IconButton className="icon-button">
+                    <ChatBubbleOutline sx = {{fontSize: 15}}/>
+                  </IconButton>
+                  <IconButton className="icon-button" >
+                    <PersonAdd sx = {{fontSize: 15}}/>
+                  </IconButton>
+                </Box>
+              </Box>
+              <Box className="tags" marginBottom={'3px'}>
                 {user.common_genre.map((genre, index) => (
-                  <Chip key={index} label={genre} className="tag" />
+                  <Chip key={index} label={genre} className="tag" size="small" color="primary" sx = {{fontSize: '10px', fontWeight:200,}}/>
                 ))}
               </Box>
               <Box className="tags">
                 {user.common_play_time.map((time, index) => (
-                  <Chip key={index} label={time} className="tag" />
+                  <Chip key={index} label={time} className="tag" size="small" color="primary" variant="outlined" sx = {{fontSize: '10px', fontWeight:200,}}/>
                 ))}
-              </Box>
-            </Grid>
-            <Grid item>
-              <Box className="icon-buttons" display={"flex"}>
-                <IconButton className="icon-button">
-                  <ChatBubbleOutline />
-                </IconButton>
-                <IconButton className="icon-button">
-                  <PersonAdd />
-                </IconButton>
               </Box>
             </Grid>
           </Grid>
