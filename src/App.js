9a2d5703a29b5f3673dafa9a52741a7cf6013c1a
[Cloud3] 김이삭 <xovnd7845@naver.com>
2024-08-06 13:38:36 +0900

    feat 이삭 테스트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import GameList from './pages/GameList/GameList';
 import GameMate from './pages/GameMate/GameMate';
 import Chat from './pages/Chat/Chat';
 import MyPage from './pages/MyPage/MyPage';
-import ChatTest from './pages/ChatTest/ChatTest';
 
 const App = () => {
     return (
