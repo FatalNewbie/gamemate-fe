d3fd5b9699a44f0bb3232416a7fa19e5a47f101d
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-08-09 06:46:33 +0900

    Refactor: 게임메이트 리스트 글쓰기 버튼 추가

--- src/pages/GameMate/GameMate.jsx
+++ src/pages/GameMate/GameMate.jsx
@@ -1,13 +1,20 @@
 import '../GameMate/GameMate.css';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import InfiniteScroll from './InfiniteScroll';
 
 const GameMate = () => {
     const [status, setStatus] = useState('on'); // 기본 상태를 'on'으로 설정
     const apiUrl = status === 'on' ? '/posts' : '/posts';
 
+    const navigate = useNavigate();
+
+    const handlePostClick = () => {
+        navigate(`/gamemate/posts/new`);
+    };
+
     return (
-        <div className="container">
+        <div className="gamemate-container">
             <div className="onoff-choice">
                 <button
                     onClick={() => setStatus('on')}
@@ -23,6 +30,9 @@ const GameMate = () => {
                 </button>
             </div>
             <InfiniteScroll status={status} apiUrl={apiUrl} />
+            <button className="write-button" onClick={() => handlePostClick()}>
+                <img src="/icons/pencil.png" alt="글쓰기" className="write-icon" />
+            </button>
         </div>
     );
 };
